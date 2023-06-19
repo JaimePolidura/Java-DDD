5743f792f04652cba51e298c61b8d3a3824b5dad
Jaime Polidura <jaime.polidura@gmail.com>
2023-06-19 14:22:16 +0200

    added atomicAcquire

--- src/main/java/es/jaime/javaddd/application/utils/ConcurrencyUtils.java
+++ src/main/java/es/jaime/javaddd/application/utils/ConcurrencyUtils.java
@@ -1,8 +1,32 @@
 package es.jaime.javaddd.application.utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.locks.Lock;
 
 public final class ConcurrencyUtils {
+    public static void atomicAcquire(Lock... locks) {
+        Queue<Lock> acquired = new LinkedList<>();
+
+        while (acquired.size() != locks.length) {
+            Lock nextLock = locks[acquired.size()];
+
+            if(nextLock.tryLock()){
+                acquired.add(nextLock);
+            }else{
+                releaseAll(acquired);
+
+                Utils.sleep((long) (Math.random() * 10));
+            }
+        }
+    }
+
+    private static void releaseAll(Queue<Lock> acquired) {
+        while (!acquired.isEmpty()) {
+            acquired.poll().unlock();
+        }
+    }
+
     public static void tryLockOnce(Lock lock, Runnable runnable) {
         if(!lock.tryLock()){
             return;
